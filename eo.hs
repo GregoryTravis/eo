5bff3ecba2c43fbe689363e04d6a4762e3785147
Gregory Travis <greg.m.travis@gmail.com>
2018-10-17 20:48:20 -0400

    only handle notes

--- eo.hs
+++ eo.hs
@@ -5,15 +5,24 @@ import System.IO
 
 sh x = do hPutStrLn stderr x
 
-logm x = if isInfixOf "note" x
+isNote x = isInfixOf "note" x
+
+logm x = if isNote x
            then sh x
            else return ()
+
+modes = [[0, 4, 7], [0, 3, 7, 10]]
+--0isCtrl x = x == 48 || x == 49
 
 guh = do
   forever $ do
     line <- getLine                                     -- line :: String
-    logm line
-    putStrLn line
+    if isNote line
+      then do
+        logm line
+        putStrLn line
+        -- putStrLn "note-on 60 127"
+      else return ()
 
 main = do
   hSetBuffering stdout NoBuffering
